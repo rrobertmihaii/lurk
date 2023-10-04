635bf474195f9423fc1d149b6caf4b5120acb305
Robert <129881539+rrobertmihaii@users.noreply.github.com>
2023-10-04 08:41:54 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+function ipserver() {
+    window.location.href = 'rage://v/connect?ip=game.liberty.mp:22005';
+}
+
+function discord() {
+    window.location.href = 'https://discord.gg/lurk';
+}
+
+function youtube() {
+    window.location.href = 'https://youtube.com/lurk';
+}
+
+function facebook() {
+    window.location.href = 'https://facebook.com/@lurk.ro';
+}
+
+function instagram() {
+    window.location.href = 'https://instagram/lurk.ro';
+}
